d6daa612e9ae619757c5f69cacc41a1a1d482b4f
serid <serridd@gmail.com>
2024-10-02 16:46:40 +0200

    update memlstd.js

--- memlstd.js
+++ memlstd.js
@@ -7,12 +7,12 @@ fun (anyways -:any): (Unit) = (Unit/C)
 
 class Array 'A end
 fun (newArray): (Array A) = native[|[]|]
-fun (push self:(Array A) x:A): (Unit)
-  = (anyways native[|self.push(x)|])
+fun (push self:(Array A) x:A): (Unit) = (anyways
+  native[|self.push(x)|])
 
 # [A]B is a type of functions from A to B.
 # Function calls are parenthesised.
 fun (let 'A 'B x:A f:[A]B): B = (f x)
-fun (write 'A x:A): (Unit) =
-  native[|console.log(x)|]
+fun (write 'A x:A): (Unit) = (anyways
+  native[|console.log(x)|])
 `
