020754e438ff4e39bcb8c49c29cf41d540525df9
jit <serridd@gmail.com>
2025-01-29 21:21:39 +0300

    Add flake.nix.

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  nixConfig.bash-prompt-suffix = "dev$ ";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs }:
+    let pkgs = nixpkgs.legacyPackages.x86_64-linux; in {
+      devShell.x86_64-linux = pkgs.mkShell {
+        packages = [ pkgs.deno ];
+    };
+  };
+}
