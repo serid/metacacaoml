47613bb7711e4ce739cd108a817f9ab3f88ce42d
jit <serridd@gmail.com>
2025-02-19 14:55:03 +0300

    Eliminate yield* from generated function calls. Because it obscures errors as "(immediate value)(immediate value) is not iterable"

--- delcont.js
+++ delcont.js
@@ -23,9 +23,24 @@ function *prompt/*<A, B>*/(comp/*: (...) -> Generator<(B -> A) -> Generator, A, 
 }
 
 // example function
-function *bar()
+function *bar() {}
 function *foo() {
     // call an effectful function, pass effects upwards, but print result
-    let res = yield* bar()
+    //desugared from: let res = yield* bar()
+    //because yield* obscures errors as
+    //"(immediate value)(immediate value) is not iterable"
+
+    //can also be generalized to ingest values as in
+    //https://chatgpt.com/share/67b646fb-c198-800e-939b-fa4f67853128
+    let g = bar()
+    let res
+    while (true) {
+      let pair = g.next()
+      if (pair.done) {
+        res = pair.value
+        break
+      }
+      yield pair.value
+    }
     console.log(res)
 }
