96a1e3568b4db4166565f651cffcf2b35472a047
jit <serridd@gmail.com>
2025-03-26 00:18:50 +0300

    Reorder and reformat meml code to a nascent codestyle.

--- std.meml.rs
+++ std.meml.rs
@@ -6,15 +6,17 @@ fun anyways(-:@any): Unit = Unit/C()
 fun and('A -:@any other:A): A = other
 
 fun id('A x:A): A = x
-fun fun('A f: A): A = f
-fun compose('A 'B 'C g: [B]C f: [A]B): [A]C =
+fun fun('A f:A): A = f
+fun compose('A 'B 'C g:[B]C f:[A]B): [A]C =
 	fun() λ x. g(f(x))
-fun compose2('A 'B 'C 'D g: [C]D f: [A B]C): [A B]D =
+fun compose2('A 'B 'C 'D g:[C]D f:[A B]C): [A B]D =
 	fun() λ x y. g(f(x y))
 
 fun let('A 'B x:A f:[A]B): B = f(x)
 fun write('A x:A): Unit = anyways(
 	native[|console.log(x)|])
+fun error(m:String): @any =
+	native[|(()=>{throw new Error(m)})()|]
 
 class Bool
 | False()
@@ -22,7 +24,7 @@ class Bool
 end
 fun Bool/from-native(b:@any): Bool =
 	native[|b?yield*_fixtures_.BoolᐅTrue():yield*_fixtures_.BoolᐅFalse()|]
-fun .then('A b: Bool onTrue: []A onFalse: []A): A =
+fun .then('A b:Bool onTrue:[]A onFalse:[]A): A =
 	Bool/elim(b onFalse onTrue)
 
 class Ordering
@@ -34,10 +36,11 @@ fun Ordering/from-lt-eq('A lt:[A A]Bool eq:[A A]Bool): [A A]Ordering =
 	fun() λ x y.
 	eq(x y).then(Ordering/Eq)
 	{ . lt(x y).then(Ordering/Lt Ordering/Gt) }
-
-let -1: Int = 0.sub(1)
+fun .to-Int(ord:Ordering): Int =
+	Ordering/elim(ord) { . -1} { . 0} { . 1}
 
 class Int end
+let -1: Int = 0.sub(1)
 fun .increment(x:Int): Int = native[|x+1|]
 fun .lt(x:Int y:Int): Bool = Bool/from-native(native[|x<y|])
 fun .eq(x:Int y:Int): Bool = Bool/from-native(native[|x===y|])
@@ -47,24 +50,18 @@ fun .sub(x:Int y:Int): Int = native[|x-y|]
 fun .mul(x:Int y:Int): Int = native[|x*y|]
 fun .div(x:Int y:Int): Int = native[|x/y|]
 
-fun .to-Int(ord:Ordering): Int =
-	Ordering/elim(ord) {. -1} {. 0} {. 1}
-
 class String end
 fun .lt(x:String y:String): Bool = Bool/from-native(native[|x<y|])
 fun .eq(x:String y:String): Bool = Bool/from-native(native[|x===y|])
 let String/cmp: [String String]Ordering =
 	Ordering/from-lt-eq(String/lt String/eq)
 
-fun error(m: String): @any =
-	native[|(()=>{throw new Error(m)})()|]
-
 class Pair 'A 'B
 | New(A B)
 end
-fun .fst('A 'B pair: Pair(A B)): A =
+fun .fst('A 'B pair:Pair(A B)): A =
 	Pair/elim(pair) { x y. x}
-fun .snd('A 'B pair: Pair(A B)): B =
+fun .snd('A 'B pair:Pair(A B)): B =
 	Pair/elim(pair) { x y. y}
 
 class Box 'A
@@ -81,15 +78,15 @@ class Option 'A
 | None()
 | Some(A)
 end
-fun .unwrap('A self: Option(A)): A =
+fun .unwrap('A self:Option(A)): A =
 	Option/elim(self)
 	{ . error("called `Option/unwrap` on a `None` value") }
 	{ x. x }
-fun .bind('A 'B self: Option(A) k: [A]Option(B)): Option(B) =
+fun .bind('A 'B self:Option(A) k:[A]Option(B)): Option(B) =
 	Option/elim(self)
 	{ . Option/None() }
 	{ x. k(x) }
-fun .map('A 'B self: Option(A) f: [A]B): Option(B) =
+fun .map('A 'B self:Option(A) f:[A]B): Option(B) =
 	self.bind(compose(Option/Some f))
 
 class Array 'A end
@@ -150,9 +147,9 @@ fun .map('A 'B i:Iter(A) f:[A]B): Iter(B) =
 	{ x. Option/Some(f(x)) }
 
 class Type end
-fun .name(t: Type): String = native[|t.fullName|]
+fun .name(t:Type): String = native[|t.fullName|]
 
-fun make-tuple-type(ts: Array(Type)): Type =
+fun make-tuple-type(ts:Array(Type)): Type =
 	Pair/elim(ts.unsnoc().unwrap()) λ init last.
 	let(init.reverse()) λ -.
 	let(Box/New(last)) λ acc.
