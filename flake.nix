b33b807389ceb5797a655be09906b2da29e2c462
jit <serridd@gmail.com>
2025-06-14 12:56:55 +0300

    Bump nixpkgs.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
 	nixConfig.bash-prompt-suffix = "dev$ ";
 
 	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 	};
 
 	outputs = { self, nixpkgs }:
