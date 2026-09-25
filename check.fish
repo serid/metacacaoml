41580761dc30b7659bc87ecf232e2d1488f0e9ac
jit <serridd@gmail.com>
2026-09-26 00:03:13 +0300

    Add `object-hash`.

--- check.fish
+++ check.fish
@@ -3,6 +3,7 @@
 # 	-not -path 'npins' \
 # 	-not -path 'node_modules' \
 # 	-not -path 'package-lock.json' \
+# 	-not -path 'src/compiler/vendor' \
 # 	-exec sed -i -E 's/\s$//g; :A; s/^(\t*)(  )/\1\t/; tA' {} +
 
 fd --unrestricted --ignore-vcs --type file \
@@ -10,6 +11,7 @@ fd --unrestricted --ignore-vcs --type file \
 	--exclude node_modules/ \
 	--exclude npins/ \
 	--exclude package-lock.json \
+	--exclude src/compiler/vendor/ \
 	--exec sed -i -E 's/\s$//g; :A; s/^(\t*)(  )/\1\t/; tA'
 
 tsgo
